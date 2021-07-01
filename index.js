0b3a813a4b5f72eb7953474c69f48d3702d9b7e1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-01 16:29:12 -0700

    add a function

---
+++ index.js
@@ -0,0 +1,9 @@
+//grab elements from the DOM
+
+//create a way to add quotes to the phrase.
+
+function quoteMaker(phrase) {
+    return `"${phrase}"`
+}
+
+console.log(quoteMaker("That sure is nice."));
